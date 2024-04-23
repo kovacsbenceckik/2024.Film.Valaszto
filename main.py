d8fc42c069d7e458db59a58a4d9aa2084a9956d2
kovacs.bence <kovacs.bence@ckik.hu>
2024-04-23 18:28:31 +0200

    1-3-ig funkciók készen vannak

--- main.py
+++ main.py
@@ -8,30 +8,56 @@ def beolvasas(cim, perc, ertekeles, melyikadat):
         perc.append(int(sor[1]))
         ertekeles.append(float(sor[2]))
         sor = fr.readline().split(": ")
-    print(cim)
-    print(perc)
-    print(ertekeles)
 
 def menu():
-    print("1. Az 5 legjobbra értékelt film")
-    print("2. Top 5 leghosszabb film")
-    print("3. Top 5 legrövidebb film")
-    print("4. Egy random film ajánlása")
-    print("5. A legrosszabb értékelést kapott film")
-    print("6. Adjá pézt :)")
+    print("1. Adjá pézt :)")
+    print("2. Az 5 legjobbra értékelt film")
+    print("3. Top 5 leghosszabb film")
+    print("4. Top 5 legrövidebb film")
+    print("5. Egy random film ajánlása")
+    print("6. A legrosszabb értékelést kapott film")
     
-    valasz = input("Milyen filmeket szeretnél kapni? (1 - 6):")
+    valasz = int(input("Milyen filmeket szeretnél kapni? (1 - 6):"))
     return valasz
 
 def melyikfajlkell():
     melyikfajl = input("Melyik fájlból szeretnéd ezt végrehajtani? (Fájlnévkiterjesztéssel!) ")
     return melyikfajl
     
-# def legjobbfilmek():
-     
-        
-        
-# def top5leghosszabb():
+def legjobbfilmek(cim, perc, ertekeles):
+    n = len(cim)
+    for i in range(n):
+        for j in range(n):
+            if ertekeles[i] > ertekeles[j]:
+                x = cim[i]
+                y = perc[i]
+                z = ertekeles[i]
+                cim[i] = cim[j]
+                perc[i] = perc[j]
+                ertekeles[i] = ertekeles[j]
+                cim[j] = x
+                perc[j] = y
+                ertekeles[j] = z 
+    print()       
+    for z in range(5):
+        print(f"Film címe: {cim[z]} - Értékelése: {ertekeles[z]}\n")
+def top5leghosszabb(cim, perc, ertekeles):
+    n = len(cim)
+    for i in range(n):
+        for j in range(n):
+            if perc[i] > perc[j]:
+                x = cim[i]
+                y = perc[i]
+                z = ertekeles[i]
+                cim[i] = cim[j]
+                perc[i] = perc[j]
+                ertekeles[i] = ertekeles[j]
+                cim[j] = x
+                perc[j] = y
+                ertekeles[j] = z   
+    print()
+    for z in range(5):
+        print(f"Film címe: {cim[z]} - Hossza: {perc[z]} perc\n")
 
 
 
@@ -42,13 +68,16 @@ def melyikfajlkell():
 # def randomfilm():
     
     
-    
-# def legrosszabb():
-    
+def legrosszabb(cim, ertekeles):
+    legk = 0
+    for i in range(1, len(ertekeles)):
+        if ertekeles[i] < ertekeles[legk]:
+            legk = i
+    return cim[legk]
 
 
-# def bongeszo():
-#     webbrowser.open('https://www.paypal.com/paypalme/benro213', new=2)
+def bongeszo():
+    webbrowser.open('https://www.paypal.com/paypalme/benro213', new=2)
 
 def main():
     cim = []
@@ -56,20 +85,21 @@ def main():
     ertekeles = []
     melyikfajl = ''
     valasz = menu()
-    if valasz == 6:
-        pass
+    if valasz == 1:
+        bongeszo()
     else:
         melyikfajl = melyikfajlkell()
     # print(melyikfajl)
         beolvasas(cim, perc, ertekeles, melyikfajl)
-        # if valasz == 1:
-            #legjobbfilmek()
-        # elif valasz == 2:
-        #     top5leghosszabb()
-        # elif valasz == 3:
-        #     top5legrovidebb()
+        if valasz == 2:
+            legjobbfilmek(cim, perc, ertekeles)
+        elif valasz == 3:
+            top5leghosszabb(cim, perc, ertekeles)
         # elif valasz == 4:
-        #     randomfilm()
+        #     top5legrovidebb()
         # elif valasz == 5:
+            # randomfilm()
+        # elif valasz == 6:
         #     legrosszabb()
+
 main()
