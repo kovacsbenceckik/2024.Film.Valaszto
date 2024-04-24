d259a516e5bc00d908bfe1f540a5588cc5f62777
kovacs.bence <kovacs.bence@ckik.hu>
2024-04-24 12:24:52 +0200

    Bug fix közös munka

--- main.py
+++ main.py
@@ -16,18 +16,39 @@ def beolvasas(cim, perc, ertekeles, melyikfajl, osszeg):
     print(f"Ez a fájl {osszeg} film adatát tartalmazza\nBetöltés...")
     sleep(0.5)
 
-def menu():
-    print("1. Netflix eredeti oldala")
-    print("2. Az 5 legjobbra értékelt film")
-    print("3. Top 5 leghosszabb film")
-    print("4. Top 5 legrövidebb film")
-    print("5. Egy random film ajánlása")
-    print("6. A legrosszabb értékelést kapott film")
-    print("7. Film hozzáadása")
-    print("8. 3 óránál hosszabb filmek")
+def menu(cim, perc, ertekeles):
+    print("1. Az 5 legjobbra értékelt film")
+    print("2. Top 5 leghosszabb film")
+    print("3. Top 5 legrövidebb film")
+    print("4. Egy random film ajánlása")
+    print("5. A legrosszabb értékelést kapott film")
+    print("6. Film hozzáadása")
+    print("7. 3 óránál hosszabb filmek")
+    print("8. Keresés első karakter megadásával")
     
     valasz = int(input("Mit szeretnél csinálni/megjeleníteni? (1 - 8):"))
-    return valasz
+    if valasz == 1:
+        legjobbfilmek(cim, perc, ertekeles)
+    elif valasz == 2:
+        top5leghosszabb(cim, perc)
+    elif valasz == 3:
+        top5legrovidebb(cim, perc)
+    elif valasz == 4:
+        randomfilm(cim)
+    elif valasz == 5:
+        legrosszabb(cim, ertekeles)
+    elif valasz == 6:
+        iras(cim, perc, ertekeles, melyikfajl)
+    elif valasz == 7:
+        cimek = haromoranaltobb(cim, perc, ertekeles)
+        print("3 óránál hosszabb filmek:")
+        for i in range(len(cimek)):
+            print()
+            print(f"{cimek[i]}")
+    elif valasz == 8:
+        karakter = input("Melyik karakterrel kezdődő filmeket keresel? ")
+        karakter = karakter.upper()
+        kereses(cim, perc, ertekeles, karakter)
 
 def melyikfajlkell():
     melyikfajl = input("Melyik fájlból szeretnéd ezt végrehajtani? (Fájlnévkiterjesztéssel!) ")
@@ -51,6 +72,7 @@ def legjobbfilmek(cim, perc, ertekeles):
     print()       
     for z in range(5):
         print(f"Film címe: {cim[z]} - Értékelése: {ertekeles[z]}\n")
+
 def top5leghosszabb(cim, perc):
     n = len(cim)
     for i in range(n):
@@ -105,18 +127,24 @@ def legrosszabb(cim, ertekeles):
     for z in range(5):
         print(f"Legrosszabbra értékelt film: {cim[z]} - Értékelése: {ertekeles[z]}")    
 
-def bongeszo():
-    webbrowser.open('https://netflix.com', new=2)
+def bennevan(elem, lista):
+    i = 0
+    while i < len(lista) and not(lista[i] == elem):
+        i += 1
+    return i < len(lista)
 
 def iras(cim, perc, ertekeles, melyikfajl):
     fw = open(melyikfajl, "a", encoding="UTF-8")
     ujcim = input("Add meg az új film címét! ")
-    ujperc = int(input("Add meg az új film hosszát percben(pl: 130)! "))
-    ujertekeles = float(input("Add meg az új film értékelését(pl: 5.8)! "))
-    cim.append(ujcim)
-    perc.append(ujperc)
-    ertekeles.append(ujertekeles)
-    fw.write(f"{ujcim}: {ujperc}: {ujertekeles}\n")
+    if not bennevan(ujcim, cim):
+        ujperc = int(input("Add meg az új film hosszát percben(pl: 130)! "))
+        ujertekeles = float(input("Add meg az új film értékelését(pl: 5.8)! "))
+        cim.append(ujcim)
+        perc.append(ujperc)
+        ertekeles.append(ujertekeles)
+        fw.write(f"{ujcim}: {ujperc}: {ujertekeles}\n")
+    else:
+        print("Ez a film már benne van a fájlban.")
     fw.close()
 
 def haromoranaltobb(cim, perc, ertekeles):
@@ -126,35 +154,21 @@ def haromoranaltobb(cim, perc, ertekeles):
             cimek.append(cim[i])
     return cimek
 
+def kereses(cim, perc, ertekeles, karakter):
+    n = len(cim)
+    for i in range(n):
+        if len(karakter) == 1 and cim[i][0] == karakter:
+            print()
+            print(cim[i])
+    menu(cim, perc, ertekeles)
+
 def main():
     cim = []
     perc = []
     ertekeles = []
-    melyikfajl = ''
     osszeg = 0
-    valasz = menu()
-    if valasz == 1:
-        bongeszo()
-    else:
-        melyikfajl = melyikfajlkell()
-        beolvasas(cim, perc, ertekeles, melyikfajl, osszeg)
-        if valasz == 2:
-            legjobbfilmek(cim, perc, ertekeles)
-        elif valasz == 3:
-            top5leghosszabb(cim, perc, ertekeles)
-        elif valasz == 4:
-            top5legrovidebb(cim, perc, ertekeles)
-        elif valasz == 5:
-            randomfilm(cim)
-        elif valasz == 6:
-            legrosszabb(cim, ertekeles)
-        elif valasz == 7:
-            iras(cim, perc, ertekeles, melyikfajl)
-        elif valasz == 8:
-            cimek = haromoranaltobb(cim, perc, ertekeles)
-            print("3 óránál hosszabb filmek:")
-            for i in range(len(cimek)):
-                print()
-                print(f"{cimek[i]}")
+    melyikfajl = melyikfajlkell()
+    beolvasas(cim, perc, ertekeles, melyikfajl, osszeg)
+    menu(cim, perc, ertekeles)
 
 main()
